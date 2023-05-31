612c2b8022c5150438b299a5885cbed0cbccefae
FelpsFranco <felipefranco.morg@outlook.com>
2023-05-31 18:22:31 -0300

    Tela Timeout / Novo Gráfico / Estrutura min-max

--- Analisedados/views.py
+++ Analisedados/views.py
@@ -4,19 +4,20 @@ from .forms import LoginForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-import random
 import datetime as dt
 from django.db.models import Sum
 from Analisedados.models import Atendimento, Atendente
+
 
 @login_required
 def graphics(request):
     if 'initial-date-inp' in request.GET and 'final-date-inp' in request.GET:
         date_initial = request.GET.get('initial-date-inp')
         date_final = request.GET.get('final-date-inp')
-        dI = dt.datetime.strptime(date_initial, "%Y-%m-%d")
-        dF = dt.datetime.strptime(date_final, "%Y-%m-%d")
-
+        print(date_initial, date_final)
+        dI = dt.datetime.strptime(date_initial, '%Y-%m-%d')
+        dF = dt.datetime.strptime(date_final, '%Y-%m-%d')
+        quality = graphics_quality(dI, dF)
         atendentes_filtrados = Atendente.objects.filter(atendimento__data__range=[dI, dF]).annotate(total_chamados=Sum('atendimento__qtd_chamados')).annotate(total_registros=Sum('atendimento__qtd_registrados'))
         if atendentes_filtrados is None:
             atendentes_filtrados = 0
@@ -25,19 +26,20 @@ def graphics(request):
         chamados_filtrados = [atendente.total_chamados or 0 for atendente in atendentes_filtrados]
         registrados_filtrados = [atendente.total_registros or 0 for atendente in atendentes_filtrados]
 
-        data = [['Analista', 'Chamados', 'Registrados']]
+        data = [['Analista', 'Jivo', 'ERP']]
         for analista, chamado, registrado in zip(analistas_filtrados, chamados_filtrados, registrados_filtrados):
             data.append([analista, chamado, registrado])
         datetime = dt.datetime.now()
-        formatted_date = datetime.strftime('%Y-%m-%d')
+        formatted_di = dI.strftime('%Y-%m-%d')
+        formatted_df = dF.strftime('%Y-%m-%d')
         infor = {
-            'dI': dI,
-            'dF': dF,
+            'data_inicial': formatted_di,
+            'data_final': formatted_df,
             'data': data,
-            'datetime': formatted_date
+            'quality': quality,
         }
-        print(infor)
         return render(request, "Analisedados/graphics.html", infor)
+
 
     else:
         atendentes = Atendente.objects.annotate(total_chamados=Sum('atendimento__qtd_chamados'))
@@ -45,18 +47,49 @@ def graphics(request):
         analistas = [atendente.nome for atendente in atendentes]
         chamados = [atendente.total_chamados for atendente in atendentes]
         registrados = [atendente.total_registros for atendente in atendentes_chamados]
-
-        data = [['Analista', 'Chamados', 'Registrados']]
+        quality = graphics_quality_not_form()
+        data = [['Analista', 'Jivo', 'ERP']]
         for analista, chamado, registrado in zip(analistas, chamados, registrados):
             data.append([analista, chamado, registrado])
         datetime = dt.datetime.now()
         formatted_date = datetime.strftime('%Y-%m-%d')
+        formatted_di = formatted_date
+        formatted_df = formatted_date
         infor = {
+            'data_inicial': formatted_di,
+            'data_final': formatted_df,
+            'quality': quality,
             'data': data,
-            'datetime': formatted_date
         }
-        print(infor)
         return render(request, "Analisedados/graphics.html", infor)
+
+
+def graphics_quality(dI, dF):
+    atendentes_filtrados = Atendente.objects.filter(atendimento__data__range=[dI, dF]).annotate(total_chamados=Sum('atendimento__positivos')).annotate(total_registros=Sum('atendimento__negativos'))
+    if atendentes_filtrados is None:
+        atendentes_filtrados = 0
+    analistas_filtrados = [atendente.nome for atendente in atendentes_filtrados]
+    positivos_filtrados = [atendente.total_chamados or 0 for atendente in atendentes_filtrados]
+    negativos_filtrados = [atendente.total_registros or 0 for atendente in atendentes_filtrados]
+
+    quality = [['Analista', 'Positivos', 'Negativos']]
+    for analista, positivo, negativo in zip(analistas_filtrados, positivos_filtrados, negativos_filtrados):
+        quality.append([analista, positivo, negativo])
+
+    return quality
+
+
+def graphics_quality_not_form():
+    atendentes = Atendente.objects.annotate(total_positivos=Sum('atendimento__positivos'))
+    atendentes_chamados = Atendente.objects.annotate(total_negativos=Sum('atendimento__negativos'))
+    analistas = [atendente.nome for atendente in atendentes]
+    positivos = [atendente.total_positivos for atendente in atendentes]
+    negativos = [atendente.total_negativos for atendente in atendentes_chamados]
+
+    quality = [['Analista', 'Positivos', 'Negativos']]
+    for analista, chamado, registrado in zip(analistas, positivos, negativos):
+        quality.append([analista, chamado, registrado])
+    return quality
 
 
 @login_required
--- alimenta_banco.py
+++ alimenta_banco.py
@@ -8,40 +8,40 @@ from Analisedados.models import Atendimento, Atendente
 
 
 
-# atendente1 = Atendente.objects.create(nome='Thomas')
-# atendente1.save()
-#
-#
-# registrados = [0, 0, 8, 10, 5, 11, 0, 0, 0, 9, 9, 8, 9, 7, 0, 0, 10, 8, 7, 9, 0, 0, 0, 9, 6, 6, 11, 9, 0, 0]
-# chamados = [0, 0, 7, 10, 2, 5, 0, 0, 0, 8, 7, 7, 7, 6, 0, 0, 6, 6, 4, 5, 0, 0, 0, 7, 4, 5, 8, 5, 0, 0]
-# qtd_positivos = [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0]
-# qtd_negativos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#
-#
-# for z, chamado in enumerate(chamados):
-#     atendimento = Atendimento(
-#         atendente=atendente1,
-#         qtd_chamados=chamado,
-#         qtd_registrados=registrados[z],
-#         positivos=qtd_positivos[z],
-#         negativos=qtd_negativos[z],
-#         data='2023-04-'+str(z+1)
-#     )
-#     atendimento.save()
+atendente1 = Atendente.objects.create(nome='John')
+atendente1.save()
+
+
+registrados = [0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 31, 0, 1, 0, 0, 0, 0]
+chamados = []
+qtd_positivos = []
+qtd_negativos = []
+
+
+for z, chamado in enumerate(chamados):
+    atendimento = Atendimento(
+        atendente=atendente1,
+        qtd_chamados=chamado,
+        qtd_registrados=registrados[z],
+        positivos=qtd_positivos[z],
+        negativos=qtd_negativos[z],
+        data='2023-04-'+str(z+1)
+    )
+    atendimento.save()
 
 
 
 
 # Mostrar todos
-registros = Atendimento.objects.all()
-for registro in registros:
-    print("Nome:", registro.atendente.nome)
-    print("Quantidade de chamados:", registro.qtd_chamados)
-    print("Quantidade registrada:", registro.qtd_registrados)
-    print("Positivos:", registro.positivos)
-    print("Negativos:", registro.negativos)
-    print("Data:", registro.data)
-    print()
+# registros = Atendimento.objects.all()
+# for registro in registros:
+#     print("Nome:", registro.atendente.nome)
+#     print("Quantidade de chamados:", registro.qtd_chamados)
+#     print("Quantidade registrada:", registro.qtd_registrados)
+#     print("Positivos:", registro.positivos)
+#     print("Negativos:", registro.negativos)
+#     print("Data:", registro.data)
+#     print()
 
 # Deletar Analista e Atendimentos
 # analista = Atendente.objects.filter(nome='Larissa')
